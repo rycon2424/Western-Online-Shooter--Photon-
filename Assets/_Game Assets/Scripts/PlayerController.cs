86f87cbc35c3c88a7b7e4f858b9a22d8d29a31f6
rycon2424 <36225966+rycon2424@users.noreply.github.com>
2019-08-03 18:10:12 +0200

    initial commit

---
+++ Assets/_Game Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerController : MonoBehaviourPun
+{
+    public float speed;
+
+    private PhotonView pv;
+
+    void Start()
+    {
+        pv = GetComponent<PhotonView>();
+        if (pv.IsMine == false)
+        {
+            return;
+        }
+    }
+
+    void Update()
+    {
+        if (pv.IsMine == false)
+        {
+            return;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(transform.forward * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(transform.forward * -speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(transform.right * -speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(transform.right * speed * Time.deltaTime);
+        }
+    }
+
+}
