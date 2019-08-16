b632b268dea242b666a217974784b5c5186df271
rycon2424 <36225966+rycon2424@users.noreply.github.com>
2019-08-16 14:58:39 +0200

    added fancy character stats select

--- Assets/_Game Assets/_Scripts/Online/CharacterSelect.cs
+++ Assets/_Game Assets/_Scripts/Online/CharacterSelect.cs
@@ -5,19 +5,31 @@ using UnityEngine.UI;
 
 public class CharacterSelect : MonoBehaviour
 {
+    [Header("Stats")]
     public int character;
-    GameSetupController gsc;
-    public Text currentselectedChar;
+    public int characterHP;
     public string characterName;
+    public string characterSpeed;
+
+    [Header("References")]
+    GameSetupController gsc;
+    public Button playButton;
+    public Text currentselectedChar;
+    public Text healthText;
+    public Text speedText;
 
     void Start()
     {
         gsc = FindObjectOfType<GameSetupController>();
+        currentselectedChar.text = "None";
     }
 
     public void ChangeCharacter()
     {
+        playButton.interactable = true;
         gsc.currentSelectedPlayer = character;
         currentselectedChar.text = characterName;
+        healthText.text = characterHP.ToString();
+        speedText.text = characterSpeed;
     }
 }
